34581dc0fb3c38cc9987db5b632a1276c3bc3d38
R3X <114203619+aryandaa@users.noreply.github.com>
2026-06-14 00:34:19 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,17 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// 1. Register The Auto Loader
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Run The Application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
